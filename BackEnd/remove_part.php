21bf354bfcaff07e56c3c908a10816a695a3ea48
Szymon3125 <50416224+Szymon3125@users.noreply.github.com>
2023-01-27 02:14:53 +0100

    changes

--- BackEnd/remove_part.php
+++ BackEnd/remove_part.php
@@ -2,7 +2,7 @@
     session_start();
     $connect = mysqli_connect("localhost", "root", "", "projekt_sql");
     mysqli_set_charset($connect, 'utf8');
-    $sql = "UPDATE konfiguracje SET " . $_POST["part_tpye"] . " = NULL WHERE Id = " . $_SESSION['config'];
+    $sql = "UPDATE konfiguracje SET " . $_POST["part_type"] . " = NULL WHERE Id = " . $_SESSION['config'];
     $result = mysqli_query($connect,$sql);
     header("Location: .\..\Pages\Subpages\config_page.php");
 ?>
